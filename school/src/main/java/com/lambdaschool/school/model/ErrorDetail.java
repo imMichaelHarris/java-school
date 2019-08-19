6c8da59c61183ab4f11e0dc9455c39764f9c6ddc
Michael Harris <mhguitar1997@gmail.com>
2019-08-19 12:35:31 -0700

    Create getters and setters for errordetail

--- school/src/main/java/com/lambdaschool/school/model/ErrorDetail.java
+++ school/src/main/java/com/lambdaschool/school/model/ErrorDetail.java
@@ -14,5 +14,51 @@ public class ErrorDetail {
     private String developerMessage;
     private Map<String, List<ValidationError>> errors = new HashMap<>();
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDeveloperMessage() {
+        return developerMessage;
+    }
+
+    public void setDeveloperMessage(String developerMessage) {
+        this.developerMessage = developerMessage;
+    }
+
+    public Map<String, List<ValidationError>> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Map<String, List<ValidationError>> errors) {
+        this.errors = errors;
+    }
 }
